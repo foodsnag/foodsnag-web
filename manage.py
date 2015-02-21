a6ec7d0931c0e5c3a7f1b2964399f1039f7292d6
CCoffie <CalebCoffie@gmail.com>
2015-02-21 13:20:57 -0500

    update management script

--- manage.py
+++ manage.py
@@ -1,51 +1,76 @@
 #!/usr/bin/env python
-import os, csv
+import os
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
+if os.path.exists('.env'):
+    print('Importing environment from .env...')
+    for line in open('.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
+
 from app import create_app, db
-from app.models import User, Location
 from flask.ext.script import Manager, Shell
+from flask.ext.migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, db=db)
+
 manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
 
 @manager.command
-def mkdb():
-  db.create_all()
+def test(coverage=False):
+    """Run the unit tests."""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
+
 
 @manager.command
-def adduser(email, username, password):
-  """Register a new user."""
-  user = User(email=email, username=username, password=password)
-  db.session.add(user)
-  db.session.commit()
+def profile(length=25, profile_dir=None):
+    """Start the application under the code profiler."""
+    from werkzeug.contrib.profiler import ProfilerMiddleware
+    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
+                                      profile_dir=profile_dir)
+    app.run()
+
 
 @manager.command
-def importschools(fpath):
-  n = 0
+def deploy():
+    """Run deployment tasks."""
+    from flask.ext.migrate import upgrade
+    from app.models import Role, User
 
-  with open(fpath) as f:
-    reader = csv.reader(f)
-    for row in reader:
-      name = row[1].replace('&amp','&')
-      if(not Location.query.filter_by(name=name).first()):
-        school = Location(name=name, url=row[2])
-        db.session.add(school)
-        n += 1
+    # migrate database to latest revision
+    upgrade()
 
-  print('%d new locations added to database' % n)
-  db.session.commit()
-
-@manager.command
-def test():
-  """Run the unit tests."""
-  import unittest
-  tests = unittest.TestLoader().discover('tests')
-  unittest.TextTestRunner(verbosity=2).run(tests)
-
+    # create user roles
+    Role.insert_roles()
 
 if __name__ == '__main__':
     manager.run()
