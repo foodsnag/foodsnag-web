92ebb452f869d852bdb29fb1d23839a516e3c012
Dan Kolbman <dankolbman@gmail.com>
2015-04-18 20:58:59 -0400

    Simplify init admin

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import sched, time, datetime
 from flask import Flask, abort, redirect, url_for
-from .extensions import db, celery, login_manager
+from .extensions import db, celery, login_manager, HomeView, UserView, EventView, LocationView
 from flask.ext.admin import Admin, BaseView, expose, AdminIndexView
 from flask.ext.admin.contrib.sqla import ModelView
 from wtforms.fields import SelectField, PasswordField
@@ -16,11 +16,14 @@ def create_app(config_name):
   app.config.from_object(config[config_name])
   config[config_name].init_app(app)
   
-  admin.init_app(app)
-
   db.init_app(app)
+
+  admin = Admin(index_view=HomeView())
+
+  admin.init_app(app)
   celery.init_app(app)
   celery.conf.CELERY_TIMEZONE = 'America/New_York'
+
 
   login_manager.session_protection = 'strong'
   login_manager.login_view = 'auth.login'
@@ -37,86 +40,4 @@ def create_app(config_name):
 
   return app
 
-# Flask-Admin Views
 
-class HomeView(AdminIndexView):
-    @expose('/')
-    def index(self):
-        print(current_user.is_authenticated())
-        if not current_user.is_authenticated():
-            return redirect(url_for('auth.login'))
-        return super(HomeView, self).index()
-
-admin = Admin(index_view=HomeView())
-
-
-# Setup More Admin view stuff
-
-class UserView(ModelView):
-    column_list = ('username', 'email', 'member_since', 'role', 'location')
-    form_excluded_columns = ('password_hash','avatar_hash')
-    column_searchable_list = ('username', 'email')
-
-    def __init__(self, session):
-        # You can pass name and other parameters if you want to
-        super(UserView, self).__init__(User, session, name="Users")
-
-
-    def is_accessible(self):
-        if current_user.can(Permission.ADMINISTER):
-            return True
-
-    def is_visible(self):
-        if current_user.can(Permission.ADMINISTER):
-            return True
-
-    def _handle_view(self, name, **kwargs):
-        if not self.is_accessible():
-            return redirect(url_for('auth.login'))
-
-class EventView(ModelView):
-
-    column_list = ( 'id', 'name', 'place', 'time', 'location', 'author'  )
-    column_searchable_list = ( 'name', 'place' )
-
-    def __init__(self, session):
-        # You can pass name and other parameters if you want to
-        super(EventView, self).__init__(Event, session, name="Events")
-
-    def is_accessible(self):
-        if current_user.can(Permission.ADMINISTER):
-            return True
-
-    def is_visible(self):
-        if current_user.can(Permission.ADMINISTER):
-            return True
-
-    def _handle_view(self, name, **kwargs):
-        if not self.is_accessible():
-            return redirect(url_for('auth.login'))
-
-class LocationView(ModelView):
-    
-    column_list = ( 'id', 'name', 'url' )
-    column_searchable_list = ( 'name', )
-
-    def __init__(self, session):
-        # You can pass name and other parameters if you want to
-        super(LocationView, self).__init__(Location, session, name="Locations")
-
-
-    def is_accessible(self):
-        if current_user.can(Permission.ADMINISTER):
-            return True
-
-    def is_visible(self):
-        if current_user.can(Permission.ADMINISTER):
-            return True
-
-    def _handle_view(self, name, **kwargs):
-        if not self.is_accessible():
-            return redirect(url_for('auth.login'))
-
-admin.add_view(UserView(db.session))
-admin.add_view(EventView(db.session))
-admin.add_view(LocationView(db.session))
